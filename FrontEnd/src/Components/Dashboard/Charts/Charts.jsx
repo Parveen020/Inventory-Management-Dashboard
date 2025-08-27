405cae96c1f701e8ebcc6e09bec21207d24d32d4
Parveen Kumar <155838628+Parveen020@users.noreply.github.com>
2025-08-27 11:40:39 +0530

    Update Charts.jsx

--- FrontEnd/src/Components/Dashboard/Charts/Charts.jsx
+++ FrontEnd/src/Components/Dashboard/Charts/Charts.jsx
@@ -9,7 +9,7 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-import "./charts.css";
+import "./Charts.css";
 import { AdminContext } from "../../../Context/AdminContext";
 
 const Charts = ({ width, height }) => {
